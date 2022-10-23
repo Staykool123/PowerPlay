9c44dc7de3f7a78c6d02b71605f9a8a22af7a16a
Staykool123 <eddywang0731@gmail.com>
2022-10-22 21:09:11 -0500

    roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-//import MecanumDriveTesting;
+import org.firstinspires.ftc.teamcode.MecanumDriveTesting;
 
 public class Teleop extends OpMode {
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawServo.java
@@ -1,39 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
 public class ClawServo extends OpMode {
     private Servo leftclaw, rightclaw;
-    public static final double open1 = 0;
-    public static final double close1 = 1.0;
-    public static final double open2 = 1.0;
-    public static final double close2 = 0;
+
+    public static final double open1 = 0.45;
+    public static final double open2 = 0.45;
+    public static final double close1 = 0.15;
+    public static final double close2 = 0.15;
 
     @Override
     public void init() {
-        telemetry.addData("Status", "Initializing...");
         leftclaw = hardwareMap.get(Servo.class,"leftclaw");
         rightclaw = hardwareMap.get(Servo.class,"rightclaw");
-
-        leftclaw.setPosition(open1);
-        rightclaw.setPosition(open2);
-
     }
 
     @Override
     public void loop() {
         if (gamepad1.x){
-            leftclaw.setPosition(open1);
-//            rightclaw.setPosition(open2);
+            rightclaw.setPosition(close2);
+            leftclaw.setPosition(close1);
+
         }
         if (gamepad1.y){
-            leftclaw.setPosition(close1);
-//            rightclaw.setPosition(close2);
+            rightclaw.setPosition(open2);
+            leftclaw.setPosition(open1);
+
         }
     }
 
