4d40a629b424feb87c45813fa97f91985e206237
Staykool123 <eddywang0731@gmail.com>
2022-11-03 19:16:52 -0500

    im eddy and i suck

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRight.java
@@ -0,0 +1,126 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous(name="BlueRight")
+public class BlueRight extends LinearOpMode {
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight, liftDrive;
+    private Servo leftclaw, rightclaw;
+    private ElapsedTime     runtime = new ElapsedTime();
+    static final double     FORWARD_SPEED = 0.4;
+    static final double     BACKWARD_SPEED = -0.4;
+    public static final double open1 = 0.45;
+    public static final double open2 = 0.45;
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variables.
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        leftclaw = hardwareMap.get(Servo.class, "leftclaw");
+        rightclaw = hardwareMap.get(Servo.class,"rightclaw");
+        liftDrive = hardwareMap.get(DcMotorEx.class,"lift");
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+
+        waitForStart();
+
+
+        frontLeft.setPower(FORWARD_SPEED);
+        frontRight.setPower(BACKWARD_SPEED);
+        backLeft.setPower(BACKWARD_SPEED);
+        backRight.setPower(FORWARD_SPEED);
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        frontLeft.setPower(FORWARD_SPEED);
+        frontRight.setPower(FORWARD_SPEED);
+        backLeft.setPower(FORWARD_SPEED);
+        backRight.setPower(FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        liftDrive.setPower(FORWARD_SPEED);
+        while (opModeIsActive() && (runtime.seconds() < 5)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        liftDrive.setPower(0);
+        leftclaw.setPosition(open1);
+        rightclaw.setPosition(open2);
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        frontLeft.setPower(BACKWARD_SPEED);
+        frontRight.setPower(BACKWARD_SPEED);
+        backLeft.setPower(BACKWARD_SPEED);
+        backRight.setPower(BACKWARD_SPEED);
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        frontLeft.setPower(FORWARD_SPEED);
+        frontRight.setPower(BACKWARD_SPEED);
+        backLeft.setPower(BACKWARD_SPEED);
+        backRight.setPower(FORWARD_SPEED);
+        while (opModeIsActive() && (runtime.seconds() < 4)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleoptesting.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp
+public class Teleoptesting extends OpMode {
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight, liftDrive;
+    private Servo leftclaw, rightclaw;
+    public static final double open1 = 0.45;
+    public static final double open2 = 0.45;
+    public static final double close1 = 0.15;
+    public static final double close2 = 0.15;
+    static final double RPM = 312;
+    double quarterTurn = RPM/4;
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        liftDrive = hardwareMap.get(DcMotorEx.class, "lift");
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftclaw = hardwareMap.get(Servo.class, "leftclaw");
+        rightclaw = hardwareMap.get(Servo.class,"rightclaw");
+        liftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftDrive.setTargetPosition(0);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        double turnRight = gamepad1.right_trigger * .75;
+        double turnLeft = gamepad1.left_trigger * .75;
+        double drive = -gamepad1.right_stick_x * .75;
+        double strafe = -gamepad1.right_stick_y;
+
+        double driveSlow = -gamepad1.left_stick_x * .3;
+        double strafeSlow = -gamepad1.left_stick_y * .3;
+
+        double liftUp = gamepad2.right_trigger;
+        double liftDown = gamepad2.left_trigger;
+
+        double frontLeftPower = Range.clip(turnRight - turnLeft - drive + strafe - driveSlow + strafeSlow, -0.7, 0.7);
+        double frontRightPower = Range.clip(turnRight - turnLeft - drive - strafe - driveSlow - strafeSlow, -0.7, 0.7);
+        double backLeftPower = Range.clip(turnRight - turnLeft + drive + strafe + driveSlow + strafeSlow, -0.7, 0.7);
+        double backRightPower = Range.clip(-turnRight + turnLeft - drive + strafe -driveSlow + strafeSlow, -0.7, 0.7);
+        double liftDrivePower = Range.clip(liftUp - liftDown, -1.0, 1.0);
+
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+        liftDrive.setPower(liftDrivePower);
+        if(gamepad1.a){
+            liftDrive.setTargetPosition((int)quarterTurn);
+            liftDrive.setPower(0.5);
+        }
+        if (gamepad2.x){
+            leftclaw.setPosition(open1);
+            rightclaw.setPosition(open2);
+        }
+        if (gamepad2.y) {
+            leftclaw.setPosition(close1);
+            rightclaw.setPosition(close2);
+        }
+
+
+        telemetry.addData("Motors", "frontLeft (%.2f), frontRight (%.2f), backLeft (%.2f), backRight(%.2f)",
+                frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+        telemetry.update();
+
+    }
+}
